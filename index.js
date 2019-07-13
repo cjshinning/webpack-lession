c7f0454a499fa269abb165ba4338cfb7ddc0b5af
cjshinning <784319417@qq.com>
2019-07-13 22:02:39 +0800

    原始代码

---
+++ index.js
@@ -0,0 +1,12 @@
+var dom = document.getElementById('root');
+var header = document.createElement('div');
+header.innerText = 'header';
+dom.appendChild(header);
+
+var sidebar = document.createElement('div');
+sidebar.innerText = 'sidebar';
+dom.appendChild(sidebar);
+
+var content = document.createElement('div');
+content.innerText = 'content';
+dom.appendChild(content);
