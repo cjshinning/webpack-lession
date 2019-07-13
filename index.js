c8f75a3199f027d75074a037da17e1cfca2b54b2
cjshinning <784319417@qq.com>
2019-07-13 22:12:04 +0800

    面向对象方式

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var dom = document.getElementById('root');
-var header = document.createElement('div');
-header.innerText = 'header';
-dom.appendChild(header);
 
-var sidebar = document.createElement('div');
-sidebar.innerText = 'sidebar';
-dom.appendChild(sidebar);
+new Header();
+new Sidebar();
+new Content();
 
-var content = document.createElement('div');
-content.innerText = 'content';
-dom.appendChild(content);
+
