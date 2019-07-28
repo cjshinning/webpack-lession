23802bf5c622e4b28b451596500e52550c750556
cjshinning <784319417@qq.com>
2019-07-28 22:56:41 +0800

    使用plugin打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+// plugin 可以在webpack运行到某个时刻的时候，帮你做一些事情
 
 module.exports = {
     mode: 'development',
@@ -57,5 +61,11 @@ module.exports = {
               ]
             }
         ]
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new CleanWebpackPlugin()
+    ]
 }
