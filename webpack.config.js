8bbb942bb02d7082adef073582cbe35065310c65
cjshinning <784319417@qq.com>
2019-09-01 17:32:37 +0800

    使用babel处理es6

--- webpack.config.js
+++ webpack.config.js
@@ -22,67 +22,77 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-              test: /\.(png|jpe?g|gif)$/,
-              use: [
-                {
-                  loader: 'url-loader',
-                  options: {
-                    //placeholder 占位符
-                    name: '[name]_[hash].[ext]',
-                    outputPath: 'images/',
-                    limit: 2048
-                  },
-                }
-              ]
-            },
-            {
-              test: /\.(eot|ttf|svg|woff)$/,
-              use: [
-                {
-                  loader: 'file-loader'
-                }
-              ]
-            },
-            {
-              test: /\.scss$/,
-              use: [
-                'style-loader',
-                {
-                  loader: 'css-loader',
-                  options: {
-                    importLoaders: 2,
-                    // modules: true
-                  }
-                },
-                'sass-loader',
-                {
-                  loader: 'postcss-loader',
-                  options: {
-                    ident: 'postcss',
-                    plugins: [
-                      require('autoprefixer')
-                    ]
-                  }
-                }
-              ]
-            },
-            {
-              test: /\.css$/,
-              use: [
-                'style-loader',
-                'css-loader',
-                {
-                  loader: 'postcss-loader',
-                  options: {
-                    ident: 'postcss',
-                    plugins: [
-                      require('autoprefixer')
-                    ]
-                  }
-                }
-              ]
+          { 
+            test: /\.js$/, 
+            exclude: /node_modules/, 
+            loader: 'babel-loader' ,
+            options: {
+              presets: [['@babel/preset-env', {
+                useBuiltIns: 'usage'
+              }]]
             }
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  //placeholder 占位符
+                  name: '[name]_[hash].[ext]',
+                  outputPath: 'images/',
+                  limit: 2048
+                },
+              }
+            ]
+          },
+          {
+            test: /\.(eot|ttf|svg|woff)$/,
+            use: [
+              {
+                loader: 'file-loader'
+              }
+            ]
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 2,
+                  // modules: true
+                }
+              },
+              'sass-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  ident: 'postcss',
+                  plugins: [
+                    require('autoprefixer')
+                  ]
+                }
+              }
+            ]
+          },
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  ident: 'postcss',
+                  plugins: [
+                    require('autoprefixer')
+                  ]
+                }
+              }
+            ]
+          }
         ]
     },
     plugins: [
