03c7145f6f0ae8ffa819aa1d73eb5fdee6f0cdfa
cjshinning <784319417@qq.com>
2019-09-07 21:51:20 +0800

    code spliting和webpack4

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -86,29 +86,10 @@ module.exports = {
       }),
       new CleanWebpackPlugin()
     ],
-    optimization: {
-      splitChunks:  {
-        chunks: 'all',
-        // minSize: 30000,
-        // maxSize: 0,
-        // minChunks: 1,
-        // maxAsyncRequests: 5,
-        // maxInitialRequests: 3,
-        // automaticNameDelimiter: '~',
-        // automaticNameMaxLength: 30,
-        // name: true,
-        // cacheGroups: {
-        //   vendors: {
-        //     test: /[\\/]node_modules[\\/]/,
-        //     priority: -10
-        //   },
-        //   default: {
-        //     minChunks: 2,
-        //     priority: -20,
-        //     reuseExistingChunk: true
-        //   }
-        // }
-      }
-    }
+    // optimization: {
+    //   splitChunks:  {
+    //     chunks: 'all'
+    //   }
+    // }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import _ from 'lodash';
+function getComponent(){
+    return import('lodash').then(({default: _}) => {
+        var element = document.createElement('div');
+        element.innerHTML = _.join(['Jenny', 'Chen'], '-');
+        return element;
+    })
+}
 
-console.log(_.join(['a','b','1c'], '***'));
-console.log(_.join(['a','b','c'], '***'));
+getComponent().then(element => {
+    document.body.appendChild(element);
+})
+
+// 代码分割，和webpack无关
+// webpack中实现代码分割两种方式：
+// 1.同步代码：只需要在webpack.common.js中做optimization配置
+// 2.异步代码（import）：无需做任何配置
