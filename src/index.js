e7376d0d6e4d0f680b555094e2d8cb02d76b1838
cjshinning <784319417@qq.com>
2019-09-14 18:03:22 +0800

    打包分析，preloading，prefetching

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-async function getComponent(){
-    const {default: _} = await import(/* webpackChunkName:"lodash" */'lodash');
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Jenny', 'Chen'], '-');
-    return element;
-}
-
 document.addEventListener('click', () => {
-    getComponent().then(element => {
-        document.body.appendChild(element);
+    import(/* webpackPrefetch: true */ './click.js').then(({default: func}) => {
+        func();
     })
 })
